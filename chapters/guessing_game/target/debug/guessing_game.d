70db788a4fe5e87278e11909f2985968d288b2ae
yash bahuguna <yashbahuguna918@gmail.com>
2025-10-05 20:31:25 +0530

    Ignore target folders in all chapters

--- chapters/guessing_game/target/debug/guessing_game.d
+++
@@ -1,1 +0,0 @@
-/Users/yashbahuguna/rust/chapters/guessing_game/target/debug/guessing_game: /Users/yashbahuguna/rust/chapters/guessing_game/src/main.rs
--- chapters/hello_world/target/debug/deps/hello_world-83b2cfe87186ac5c.d
+++
@@ -1,5 +0,0 @@
-/Users/yashbahuguna/rust/chapters/hello_world/target/debug/deps/hello_world-83b2cfe87186ac5c.d: src/main.rs
-
-/Users/yashbahuguna/rust/chapters/hello_world/target/debug/deps/hello_world-83b2cfe87186ac5c: src/main.rs
-
-src/main.rs:
