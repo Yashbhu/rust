03b2e870d436e2c5ccb163f1dcb61fd6ed6b81b1
yash bahuguna <yashbahuguna918@gmail.com>
2025-10-07 00:05:42 +0530

    done this chpter

--- chapters/secret_key/src/main.rs
+++ chapters/secret_key/src/main.rs
@@ -1,15 +1,15 @@
 use std::io;
 use rand::Rng;
 
-fun main(){
-    println("guess your number bro!");
-    let secret = rand::thread::rng().gen_range(1..=100);
+fn main() {
+    println!("guess your number bro!");
+    let secret = rand::thread_rng().gen_range(1..=100);
     println!("the secret number is :{secret}");
      
-    let mut guess = string::new();
+    let mut guess = String::new();
 
-    io::stdin().readline(&mut guess)
+    io::stdin().read_line(&mut guess)
     .expect("it failed to read line");
-    println!("your guess: {guess}");ß
+    println!("your guess: {guess}");
 
 }
